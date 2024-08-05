dd1865b9c68b95157d59a4b159ff11f5afd15bb1
vtcong2299 <vtcong2299@gmail.com>
2024-08-05 22:23:17 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+namespace KhaiBaoVaSuDungBien
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Nhap ten cua ban: ");
+          string name= Console.ReadLine();
+          Console.WriteLine("Xin chao {0} ", name);
+        }
+    }
+}
